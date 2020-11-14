28725dd5de84ef837da309f130d53f2af73b1c72
nozomi.mori <nozomi22189@gmail.com>
2020-11-14 02:46:03 +0000

    heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :show, :edit]
   
   def index
     @task = current_user.tasks.build
