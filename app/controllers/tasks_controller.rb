86d5f94db0e19295ea69c3b7b84427691783f1ea
nozomi.mori <nozomi22189@gmail.com>
2020-11-16 11:50:33 +0000

    correct syuusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :show, :edit]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
     @task = current_user.tasks.build
